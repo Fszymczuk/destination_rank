55b5b289e937533b6fd9f7678f9a134d3203414c
Filip Szymczuk <szymczuk.dev@gmail.com>
2023-01-30 21:51:46 +0100

    add auth funcionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:destination_rank/features/auth/pages/auth_gate.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'home/pages/home_paqe.dart';
+import 'features/home/pages/home_paqe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,27 +18,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection("categories").snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Text('blad');
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text('ladowanie');
-          }
-
-          final documents = snapshot.data!.docs;
-
-          return ListView(
-            children: [
-              Categorywidget(documents[0]['title']),
-              const Categorywidget('aaa'),
-            ],
-          );
-        },
-      ),
-    );
+    return const AuthGate();
   }
 }
