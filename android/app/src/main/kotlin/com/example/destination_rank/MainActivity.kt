d650d31e8529f2affe64da90b929ef625c38d7df
Filip Szymczuk <szymczuk.dev@gmail.com>
2023-01-30 19:57:55 +0100

    initialize repository

---
+++ android/app/src/main/kotlin/com/example/destination_rank/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.filipszymczuk.destination_rank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+ );
+  runApp(const MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(body:  StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("categories").snapshots(),
+      builder: (context, snapshot) {
+
+if (snapshot.hasError) {return const Text('blad');}
+if (snapshot.connectionState== ConnectionState.waiting) {return const Text('ladowanie');}
+
+final documents = snapshot.data!.docs;
+
+        return ListView(
+          children:  [
+            Categorywidget(documents[0]['title']),
+            Categorywidget('aaa'),
+            Categorywidget('aaa'),
+          ],
+        );
+      }
+    ),);
+  }
+}
+
+class Categorywidget extends StatelessWidget {
+  const Categorywidget(this.title,{
+    Key? key,
+  }) : super(key: key);
+
+final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(color: Colors.red,child:  Text(title),),
+    );
+  }
+}
+
+
+git config --global user.name "Filip Szymczuk"
+git config --global user.email szymczuk.dev@gmail.com
