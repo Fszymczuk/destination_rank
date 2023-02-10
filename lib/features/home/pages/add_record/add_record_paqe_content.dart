3303abd26d3adb7cb20426c1b72b17e3255c48ff
Filip Szymczuk <szymczuk.dev@gmail.com>
2023-02-10 12:04:50 +0100

    add page cubit

--- lib/features/home/pages/add_record/add_record_paqe_content.dart
+++ lib/features/home/pages/add_record/add_record_paqe_content.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:destination_rank/features/home/pages/add_record/cubit/add_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddRecordPaqeContent extends StatefulWidget {
   const AddRecordPaqeContent({
@@ -12,64 +14,93 @@ class AddRecordPaqeContent extends StatefulWidget {
 }
 
 class _AddRecordPaqeContentState extends State<AddRecordPaqeContent> {
-  var countryName = '';
-  var cityName = '';
-  var rank = 10.0;
+  String? _countryName;
+  String? _cityName;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (newValue) {
+    return BlocProvider(
+      create: (context) => AddCubit(),
+      child: BlocBuilder<AddCubit, AddState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Add new upcoming title'),
+              actions: [
+                IconButton(
+                  onPressed: _countryName == null || _cityName == null
+                      ? null
+                      : () {
+                          context.read<AddCubit>().add(
+                                _countryName!,
+                                _cityName!,
+                              );
+                        },
+                  icon: const Icon(Icons.check),
+                ),
+              ],
+            ),
+            body: _AddPageBody(
+              onContryChanged: (newValue) {
                 setState(() {
-                  countryName = newValue;
+                  _countryName = newValue;
                 });
               },
-              decoration: const InputDecoration(hintText: 'Type Country'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (newValue) {
+              onCityChanged: (newValue) {
                 setState(() {
-                  cityName = newValue;
+                  _cityName = newValue;
                 });
               },
-              decoration: const InputDecoration(hintText: 'Type City'),
             ),
-          ),
-          Slider(
-              label: rank.toString(),
-              divisions: 9,
-              min: 1.0,
-              max: 10.0,
-              value: rank,
-              onChanged: ((newValue) {
-                setState(() {
-                  rank = newValue;
-                });
-              })),
-          ElevatedButton(
-              onPressed: countryName.isEmpty || cityName.isEmpty
-                  ? null
-                  : (() {
-                      FirebaseFirestore.instance.collection('records').add({
-                        'country': countryName,
-                        'city': cityName,
-                        'rank': rank,
-                      });
-                      widget.onSave();
-                    }),
-              child: const Text('Add'))
-        ],
+          );
+          ;
+        },
       ),
+    );
+  }
+}
+
+class _AddPageBody extends StatelessWidget {
+  const _AddPageBody({
+    Key? key,
+    required this.onContryChanged,
+    required this.onCityChanged,
+  }) : super(key: key);
+
+  final Function(String) onContryChanged;
+  final Function(String) onCityChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 30,
+        vertical: 20,
+      ),
+      children: [
+        TextField(
+          onChanged: onContryChanged,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Matrix 5',
+            label: Text('Title'),
+          ),
+        ),
+        const SizedBox(height: 20),
+        TextField(
+          onChanged: onCityChanged,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'http:// ... .jpg',
+            label: Text('Image URL'),
+          ),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Choose release date'),
+        ),
+      ],
     );
   }
 }
